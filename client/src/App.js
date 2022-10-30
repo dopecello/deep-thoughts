a5ecdeea28e7aa700bbcb8e3300bd3d4c62c70f8
dopecello <williamlocke.cello@gmail.com>
2022-10-30 19:42:50 -0400

    create thoughtlist component... displayed thoughts

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
-import Home from './pages/Home';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
