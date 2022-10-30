257c960487f2a06317b41cc6fc1cf2a1e7607cd7
dopecello <williamlocke.cello@gmail.com>
2022-10-30 11:55:19 -0400

    authorize users with jwts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -26,7 +27,8 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
-      return user;
+      const token = signToken(user);
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
@@ -37,7 +39,8 @@ const resolvers = {
       if (!correctPw) {
         throw new AuthenticationError("Incorrect credentials");
       }
-      return user;
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
