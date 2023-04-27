9da51502965c9acba32bac221ce79ba8af349d6b
dopecello <williamlocke.cello@gmail.com>
2023-04-27 12:07:55 -0400

    deploying mongodb try4

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -31,7 +30,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
 
   db.once("open", () => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
