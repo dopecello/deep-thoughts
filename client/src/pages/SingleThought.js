3e79c6da3bda088006ddbb09fcd15181715d91d1
dopecello <williamlocke.cello@gmail.com>
2023-04-27 10:55:55 -0400

    added reaction component and added functionality

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import { QUERY_THOUGHT } from "../utils/queries";
-import ReactionList from "../components/ReactionList";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
+import auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
@@ -15,20 +17,21 @@ const SingleThought = (props) => {
   }
   return (
     <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
+      <div className='card mb-3'>
+        <p className='card-header'>
+          <span style={{ fontWeight: 700 }} className='text-light'>
             {thought.username}
-          </span>{" "}
+          </span>{' '}
           thought on {thought.createdAt}
         </p>
-        <div className="card-body">
+        <div className='card-body'>
           <p>{thought.thoughtText}</p>
         </div>
       </div>
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+      {auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
