21dd86668457bb5ab2cc5acdecf6076e1af83cb4
dopecello <williamlocke.cello@gmail.com>
2023-04-27 12:17:36 -0400

    deploy try 5

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/deep-thoughts",
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   }
 );
 
--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
 
   db.once("open", () => {
-    app.listen(process.env.PORT || 3000, () => {
+    app.listen(process.env.PORT || 3001, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
